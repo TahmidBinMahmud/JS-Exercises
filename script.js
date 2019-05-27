d5547849016af6dfc66c2387e7273b1e1cb7eed5
cis101 <cis101@secs.iub.edu.bd>
2019-05-27 09:29:50 +0600

    Task 3

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function sum(arr){
 }
 
 console.log("Array Sum: " + sum([5, 10, 15, 20]));
+
+// Task 3: Email Validation with reg expression
+
+function validateEmail(email){
+// Regular Expression to validate proper email address
+// /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+
+console.log("Email: yellow.com is " + validateEmail("yellow.com"));
+console.log("Email: 1510927@iub.edu.bd is " + validateEmail("1510927@iub.edu.bd"));
+console.log("Email: rifatmahmud196@gmail.com is " + validateEmail("rifatmahmud196@gmail.com"));
