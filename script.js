b3ccd5a118d62398e31daa1400c17be006144672
cis101 <cis101@secs.iub.edu.bd>
2019-05-27 09:14:04 +0600

    Task 2

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /* JavaScript code here */
 console.log("External Javascript");
+
+// Task 2: Array Sum
+
+function sum(arr){
+    var sum = 0;
+    arr.forEach(function(item, index){
+        sum = sum + item;
+    })
+    return sum;
+}
+
+console.log("Array Sum: " + sum([5, 10, 15, 20]));
